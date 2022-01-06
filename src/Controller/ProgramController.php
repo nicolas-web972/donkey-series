d92eb1521aa1d52123c1049680692c2ade42e8c7
Nicolas Chalono <nicolas.chalono@gmail.com>
2022-01-06 16:03:18 +0100

    findby from dbb

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -2,21 +2,44 @@
 
 namespace App\Controller;
 
+use App\Repository\ProgramRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/program/', name: 'program_')]
+#[Route('/program', name: 'program_')]
 
 class ProgramController extends AbstractController
 {
-    #[Route('{id}', methods: ['get'], name: 'show',  requirements: ['id' => '\d+'])]
-    public function show(int $id = 1): Response
+    private $programRepository;
+
+    public function __construct(ProgramRepository $programRepository)
     {
-        return $this->render('program/show.html.twig', [
-            'id' => $id
-        ]);
+        $this->programRepository = $programRepository;
     }
+
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        $programs = $this->programRepository->findAll();
+
+        return $this->render('program/index.html.twig', compact('programs'));
+    }
+
+    #[Route('/{id}', methods: ['get'], requirements: ['id' => '\d+'], name: 'show')]
+    public function show(int $id): Response
+    {
+        $program = $this->programRepository->findOneBy(['id' => $id]);
+
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'No program with id : ' . $id . ' found in program\'s table.'
+            );
+        }
+
+        return $this->render('program/show.html.twig', compact('program'));
+    }
+
     #[Route('{param}', methods: ['get'], requirements: ['param' => '.*'], name: 'notResponse')]
     public function notResponse(): Response
     {
